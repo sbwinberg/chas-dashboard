c106b5c789acd8b23321a70df17b9f18df0f5f93
Emil <emil.winberg1@gmail.com>
2023-12-09 19:37:05 +0100

    commented code, removed unneccesary variables and other cleanup

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
-// TODO
-
-    
-// ADD FUNCTION TO LOAD ALL ELEMENTS FROM LOCAL STORAGE
-// SET LINKS, NOTES, TITLE ETC
+// FUNCTION LOAD FROM LOCAL STORAGE
+///////////////////////////////////
 window.addEventListener('load', addFromStorage);
 
 function addFromStorage() {
-    // If there's a value in localStorage, set title accordingly
     if(localStorage.getItem('title')){
         title.innerText = localStorage.getItem('title');
     }
@@ -24,8 +20,8 @@ function addFromStorage() {
     setInterval(setTime, 1000);
 }
 
-
-// FUNCTION FOR SETTING TIME AND DATE
+// FUNCTION SET TIME AND DATE
+/////////////////////////////
 const timeArea = document.querySelector('.time')
 const dateArea = document.querySelector('.date')
 
@@ -34,7 +30,6 @@ function setTime() {
     const timeString = timeNow.toLocaleTimeString();
     timeArea.innerText = timeString;
 }
-
 
 function setDate() {
     const months = ['December','Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November']
@@ -46,17 +41,19 @@ function setDate() {
     dateArea.innerText = `${day} ${month} ${year}`;
 }
 
-
-// FUNCTION CHANGE TITLE ON DOUBLE CLICK
+// FUNCTION CHANGE TITLE
+/////////////////////////////////////////
 const title = document.querySelector('.title');
 const titleInput = document.querySelector('.title-input')
 
+// HIDE TITLE AND SHOW INPUT ON DOUBLE CLICK
 title.addEventListener('dblclick', () => {
     titleInput.classList.remove('hidden');
     title.classList.add('hide')
     titleInput.focus();
 })
 
+// WHEN ENTER IF PRESSED OR INPUT OUTFOCUSED SET AND SAVE TITLE
 titleInput.addEventListener('keydown', (e) => {
     if(e.keyCode === 13){
         updateTitle();
@@ -67,6 +64,7 @@ titleInput.addEventListener('focusout', () => {
     updateTitle();
 })
 
+// UPDATE HTML AND LOCAL STORAGE + DEFAULT TITLE
 function updateTitle(){
     let newTitle = titleInput.value;
     localStorage.setItem('title', newTitle)
@@ -79,39 +77,53 @@ function updateTitle(){
     }
 }
 
-
 // FUNCTION ADD LINKS
+/////////////////////
 const linkList = document.querySelector('.links');
 const addLinkBtn = document.querySelector('.add-link');
 const linkInput = document.querySelector('.link-input');
-const regexURL = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+const nameInput = document.querySelector('.name-input')
+const regexURL = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 let links = [];
+let url;
 
-
+// DISPLAY INPUT ON DOUBLE CLICK
 addLinkBtn.addEventListener('click', () => {
     linkInput.classList.remove('hidden');
     addLinkBtn.classList.add('hide')
     linkInput.focus();
 })
 
-// FIX MESSY ASS IF STATEMENTS
+// ON ENTER AND VALID INPUT SAVE URL VALUE AND DISPLAY NEXT INPUT
 linkInput.addEventListener('keydown', (e) => {
     if(e.keyCode != 13) return
+
     url = linkInput.value.toLowerCase();
-    
     if(url == ''){
         linkInput.classList.add('hidden');
-        return
-    }
-    
-    if(regexURL.test(url)){
+        addLinkBtn.classList.remove('hide');
+    } else if(!regexURL.test(url)){
         linkInput.value = '';
-        linkInput.placeholder = 'Enter a name:'
-        
-    }else if(linkInput.placeholder == 'Enter a name:'){
-        links = JSON.parse(localStorage.getItem('links'));
+        alert('Invalid link format, try again!');
+        addLinkBtn.classList.remove('hide')
+    } else {
+        linkInput.value = '';
+        linkInput.classList.add('hidden')
+        nameInput.classList.remove('hidden')
+        nameInput.focus();
+    }
+})
 
-        let name = linkInput.value;
+// ON ENTER AND VALID INPUT SAVE URL AND NAME IN LOCAL STORAGE THEN DISPLAY LINK
+nameInput.addEventListener('keydown', (e) => {
+    if(e.keyCode != 13) return
+    
+    let name = nameInput.value;
+    if(name == ''){
+        nameInput.classList.add('hidden');
+    } else {
+        links = JSON.parse(localStorage.getItem('links'));
+    
         let link = {
             'link' : `${url}`,
             'name' : `${name}`
@@ -119,20 +131,29 @@ linkInput.addEventListener('keydown', (e) => {
         links.push(link);
         storageLinks = JSON.stringify(links)
         localStorage.setItem('links', storageLinks)
+        nameInput.value = ''
+        nameInput.classList.add('hidden');
+        displayLink(url, name)
+    }    
+})
+
+// HIDE INPUT WHEN UNFOCUSING
+// ONLY SHOW ADD BUTTON IF NAME INPUT IS NOT VISIBLE
+linkInput.addEventListener('focusout', () => {
+    if(nameInput.classList.contains('hidden')){
         linkInput.value = ''
         linkInput.classList.add('hidden');
-        displayLink(url, name)
-        linkInput.placeholder = 'Enter URL:'
+        addLinkBtn.classList.remove('hide')
     }
 })
 
-linkInput.addEventListener('focusout', () => {
-    linkInput.value = ''
-    linkInput.placeholder = 'Enter URL:'
-    linkInput.classList.add('hidden')
+nameInput.addEventListener('focusout', () => {
+    nameInput.value = '';
+    nameInput.classList.add('hidden');
+    addLinkBtn.classList.remove('hide');
 })
 
-
+// GET FAVICON AND ADD HTML TEMPLATE FOR LINKS
 function displayLink(url, name) {
     // WORKS BUT WITH LOW RESOLUTION
     // <img src="http://www.google.com/s2/favicons?domain_url=${url}" alt="${name} icon" class="icon">
@@ -156,6 +177,8 @@ function displayLink(url, name) {
 }
 
 // FUNCTION CLOSE BUTTON
+////////////////////////
+// REMOVE ELEMENT AND FILTER LOCAL STORAGE
 // NOT AN OPTION TO HAVE TWO IDENTICAL LINKS
 linkList.addEventListener('click', (e) => {
     if(e.target.classList.contains('close')){
@@ -163,40 +186,44 @@ linkList.addEventListener('click', (e) => {
         links = links.filter((obj) => {
             return obj.link != e.target.getAttribute('data-id')
         })
-        console.log(links);
         links = JSON.stringify(links);
         localStorage.setItem('links', links)
         e.target.parentElement.remove();
     }
 });
 
-
-// NOTES
-// SAVES ON FOCUS OUT, UPDATING VIA KEYBOARD LOSES EVERYTHING WRITTEN
+// FUNCTION NOTES
+/////////////////
+// SAVES ON FOCUS OUT, UPDATING VIA KEYBOARD LOSES EVERYTHING NOT SAVED
 const text_area = document.querySelector('.text-area');
 text_area.addEventListener('focusout', () => {
     const notes = text_area.value;
     localStorage.setItem('notes', notes);
 })
 
+// SAVES EVERY KEYPRESS BUT MISSES LAST KEY
+text_area.addEventListener('keydown', () => {
+    const notes = text_area.value;
+    localStorage.setItem('notes', notes);
+})
 
 // FUNCTION WEATHER
 ///////////////////
-
-// Get position via geolocation api
+// GET POSITION VIA BROWSER API
 navigator.geolocation.getCurrentPosition((position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     getWeather(lat, lon)
 });
 
-
+// FETCH WEATHER FORECAST FROM WEATHER API 
 async function getWeather(lat = 0, lon = 0, url = `https://api.weatherapi.com/v1/forecast.json?key=25a21f44ab1f402584c160402232711&q=${lat},${lon}&days=3&lang=sv`){
     const response = await fetch(url)
     const data = await response.json();
     displayWeather(data)
 }
 
+// GET WEATHER WITH NEW LOCATION ON ENTER
 const city = document.querySelector('.location-input');
 city.addEventListener('keydown', (e) => {
     if(e.keyCode != 13) return
@@ -204,6 +231,7 @@ city.addEventListener('keydown', (e) => {
     getWeather(0, 0, `https://api.weatherapi.com/v1/forecast.json?key=25a21f44ab1f402584c160402232711&q=${newPlace}&days=3&lang=sv`)
 })
 
+// DISPLAY WEATHER FROM WEATHER API
 function displayWeather(data){
     const weekdays = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag']
     const container = document.querySelector('.all-weathers')
@@ -240,76 +268,75 @@ function displayWeather(data){
     }
 }
 
-
 // FUNCTION BACKGROUND IMAGE
-let data;
+////////////////////////////
+// GET DATA FROM UNSPLASH API
 async function getImage(url = `https://api.unsplash.com/photos/random?query=wallpaper&count=1&orientation=landscape&client_id=eTEJDG-hpE5fzr60ehDGE_qEifMHQXo1Da67SzTYRr4`) {
     const response = await fetch(url);
-    data = await response.json();
+    const data = await response.json();
+    console.log(data);
     displayBackground(data)
 }
 
-
+// CUT IMAGE TO CORRECT DIMENSIONS BASED ON WINDOW SIZE AND UPDATE CSS ROOT VARIABLE
 function displayBackground(data) {
     const w = window.innerWidth;
     const h = window.innerHeight;
-    const index = Math.floor(Math.random() * data.length);
-    const bg = `url('${data[index].urls.raw}&h=${h}&w=${w}&dpr=2')`;
+    const bg = `url('${data[0].urls.raw}&h=${h}&w=${w}&dpr=2')`;
     document.body.style.setProperty('--bg-img', bg);
 }
 
-
+// GET NEW IMAGE ON CLICK
 const bgBtn = document.querySelector('.new-background');
 bgBtn.addEventListener('click', () => {
     getImage();
 })
 
-
 // FUNCTION BG QUERY
 // UNBSPLASH RATE LIMIT 50 PICS/HOUR
+////////////////////////////////////
 const queryBtn = document.querySelector('.query-btn');
 const queryInput = document.querySelector('.query-input')
 
-
+// SHOW INPUT ON CLICK
 queryBtn.addEventListener('click', () => {
     queryInput.classList.remove('hidden');
     queryBtn.classList.add('hide');
     queryInput.focus();
 })
 
+// REMOVE INPUT WHEN UNFOCUS
 queryInput.addEventListener('focusout', () => {
     queryInput.value = '';
     queryInput.classList.add('hidden');
     queryBtn.classList.remove('hide');
 })
 
-
+// ON ENTER AND VALID INPUT GET NEW IMAGE FROM API WITH INPUT QUERY
 queryInput.addEventListener('keydown', (e) => {
-    if(e.keyCode === 13){
-        if(queryInput.value == ''){
-            queryInput.classList.add('hidden')
-        } else {
-            let query = queryInput.value;
-            // change count to 1 to not overload my api limit :(
-            let url = `https://api.unsplash.com/photos/random?query=${query}&count=1&orientation=landscape&client_id=eTEJDG-hpE5fzr60ehDGE_qEifMHQXo1Da67SzTYRr4`
-            getImage(url);
-        
-            queryInput.value = '';
-            queryInput.classList.add('hidden');
-        }
+    if(e.keyCode != 13) return 
+
+    if(queryInput.value == ''){
+        queryInput.classList.add('hidden')
+    } else {
+        let query = queryInput.value;
+        let url = `https://api.unsplash.com/photos/random?query=${query}&count=1&orientation=landscape&client_id=eTEJDG-hpE5fzr60ehDGE_qEifMHQXo1Da67SzTYRr4`
+        queryInput.value = '';
+        queryInput.classList.add('hidden');
+        getImage(url);
     }
 })
 
-
 // FUNCTION CAT FACTS API
+/////////////////////////
+// GET DATA FROM API
 async function getCatFact() {
     const response = await fetch('https://cat-fact.herokuapp.com/facts');
     const data = await response.json();
     displayCatFact(data)
-}
-getCatFact();
+} getCatFact();
 
-
+// DISPLAY RANDOM ONE OF AVAILABLE CAT FACTS
 function displayCatFact(data) {
     const factP = document.querySelector('.cat-fact');
     const index = Math.floor(Math.random() * data.length);
